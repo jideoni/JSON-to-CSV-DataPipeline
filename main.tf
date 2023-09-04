c7681319e353c9f95b024d2b2ec73174757f1308
jideoni <127896618+jideoni@users.noreply.github.com>
2023-09-05 00:45:38 +0100

    Update main.tf created an S3 bucket

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
+#data "aws_ami" "app_ami" {
+#  most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
+#  filter {
+#    name   = "name"
+#    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+#  }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#  filter {
+#    name   = "virtualization-type"
+#    values = ["hvm"]
+#  }
+#
+#  owners = ["979382823631"] # Bitnami
+#}
 
-  owners = ["979382823631"] # Bitnami
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t2.micro"
+resource "aws_s3_bucket" "example" {
+  bucket = "bucket-for-json-objects"
 
   tags = {
-    Name = "HelloWorld"
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
