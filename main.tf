63f168ee12cc52cb32f96d4c43d60bb54399ed02
jideoni <127896618+jideoni@users.noreply.github.com>
2023-09-05 18:44:01 +0100

    s3 permissions for lambda

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ data "aws_iam_policy_document" "lambda_s3_permissions" {
         "s3-object-lambda:*",
       ]
 
-      resources = ["arn:aws:s3:*:*:*"]
+      resources = ["arn:aws:s3:::*/*"]
   }
 }
 
