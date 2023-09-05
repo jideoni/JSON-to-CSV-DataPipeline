d6cd91e1d524abe6c00c41048b76e74d4d021847
jideoni <127896618+jideoni@users.noreply.github.com>
2023-09-05 17:34:08 +0100

    email subscription to sns

--- main.tf
+++ main.tf
@@ -139,3 +139,10 @@ resource "aws_s3_bucket_notification" "csv_bucket_trigger_sns" {
     filter_suffix = ".csv"
   }
 }
+
+#create subscription for email
+resource "aws_sns_topic_subscription" "email_target" {
+  topic_arn = "arn:aws:sns:us-east-1:380255901104:JSON_to_CSV_conversion_complete"
+  protocol  = "email"
+  endpoint  = ["onibabajide34@gmail.com"]
+}
