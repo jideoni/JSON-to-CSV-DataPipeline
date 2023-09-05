d0fb321621680027870a1e9853f31d52b6a00424
jideoni <127896618+jideoni@users.noreply.github.com>
2023-09-05 02:15:53 +0100

    commit

--- main.tf
+++ main.tf
@@ -1,25 +1,11 @@
 #data "aws_ami" "app_ami" {
-#  most_recent = true
-
-#  filter {
-#    name   = "name"
-#    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-#  }
-
-#  filter {
-#    name   = "virtualization-type"
-#    values = ["hvm"]
-#  }
-#
-#  owners = ["979382823631"] # Bitnami
+  
 #}
 
 resource "aws_s3_bucket" "json-bucket" {
   bucket = "bucket-for-json-objects"
-  acl = "private"
 }
 
 resource "aws_s3_bucket" "csv-objects" {
   bucket = "bucket-for-csv-objects"
-  acl = "private"
 }
