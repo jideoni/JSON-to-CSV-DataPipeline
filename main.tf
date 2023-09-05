b94ec158a01cb5251df25bd606f0fec4c70b087c
jideoni <127896618+jideoni@users.noreply.github.com>
2023-09-05 17:55:41 +0100

    corrected lambda function name

--- main.tf
+++ main.tf
@@ -86,7 +86,7 @@ resource "aws_lambda_function" "csv_to_json_lambda" {
   filename      = "lambda_function_payload.zip"
   function_name = var.lambda_function_name
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "lambda_handler"
+  handler       = "lambda_function.lambda_handler"
 
   source_code_hash = data.archive_file.lambda-function.output_base64sha256
 
