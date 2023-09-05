80dc62b3a6dade4656eb6d15349289553d5619ec
jideoni <127896618+jideoni@users.noreply.github.com>
2023-09-05 15:59:03 +0100

    d

--- main.tf
+++ main.tf
@@ -11,36 +11,10 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-data "aws_iam_policy_document" "lambda_logging" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-    ]
-
-    resources = [aws_lambda_function.csv_to_json_lambda.arn]
-  }
-}
-
-resource "aws_iam_policy" "lambda_logging" {
-  name        = "lambda_logging"
-  path        = "/"
-  description = "IAM policy for logging from a lambda"
-  policy      = data.aws_iam_policy_document.lambda_logging.json
-}
-
 #create an iam role for lambda
 resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.lambda_logging.arn
 }
 
 data "archive_file" "lambda-function" {
@@ -76,9 +50,4 @@ resource "aws_lambda_function" "csv_to_json_lambda" {
     #aws_iam_role_policy_attachment.lambda_logs,
     #aws_cloudwatch_log_group.CSV_to_JSON-function-log-group,
   #]
-}
-
-resource "aws_cloudwatch_log_group" "CSV_to_JSON-function-log-group" {
-  name              = "/aws/lambda/${var.lambda_function_name}"
-  retention_in_days = 30
 }
