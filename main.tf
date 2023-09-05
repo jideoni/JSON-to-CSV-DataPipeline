929867146114585f0fc215959109c89c9968c3f2
jideoni <127896618+jideoni@users.noreply.github.com>
2023-09-05 18:46:58 +0100

    s3

--- main.tf
+++ main.tf
@@ -22,8 +22,6 @@ data "aws_iam_policy_document" "lambda_logging" {
       "s3:GetObject",
       "s3:PutObject",
       "s3:CreateBucket",
-      "s3:*",
-      "s3-object-lambda:*",
     ]
 
     resources = ["arn:aws:logs:*:*:*"]
