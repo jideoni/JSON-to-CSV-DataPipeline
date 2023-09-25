02ef1115cfe8b6e2120f223a15c68e329924bd06
Babajide Oni <127896618+jideoni@users.noreply.github.com>
2023-09-24 21:38:52 -0500

    Provider update

--- providers.tf
+++ providers.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.37.0"
+      version = "~> 5.0"
     }
   }
-  required_version = ">= 3.33.0"
+  required_version = "~> 3.0"
 }
 
 provider "aws" {
