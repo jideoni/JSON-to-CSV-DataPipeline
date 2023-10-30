2f6d12b998cf10ba7aab54bdabe828878172b04f
Babajide Oni <127896618+jideoni@users.noreply.github.com>
2023-10-30 04:31:50 +0100

    Clean up

--- lambda_function.py
+++ lambda_function.py
@@ -13,73 +13,31 @@ csv_object_name = os.environ['csv_object_name']
 
 
 response = ""
+#s3 client
 s3 = boto3.client('s3')
 #sqs client
 sqs = boto3.client('sqs')
 
 def lambda_handler(event, context):
-    #print(event)
     #print("Received event: " + json.dumps(event, indent=2))
-
     buildCSV = ""
     # Get the object from the event and show its content type
-
-    #Retrieve object body
     records = event['Records'][0]
-    #records_body = records.get('body')
-    #print(records)
-    #print(type(records))
-    #v = records[0]      #first item in the list of records
     body_in_string = records.get('body')
     body_in_json = json.loads(body_in_string)
     s3_records = body_in_json['Records'][0]
     s3_details = s3_records.get('s3')
-
-
-    '''for j in event.values():
-        for x in j:
-            #print(x)
-            for a,b in x.items():
-                if a == 'body':
-                    body_in_string = b
-                    body_in_json = json.loads(body_in_string)'''
     
     #Retrieve bucket name
+    #bucket = body_in_json['Records'][0]['s3']['bucket']['name']
     s3_bucket = s3_details.get('bucket')
     bucket = s3_bucket.get('name')
-    
-    #bucket = body_in_json['Records'][0]['s3']['bucket']['name']
-
-    '''for j in body_in_json.values():
-        for x in j:
-            #print(x)
-            for a,b in x.items():
-                if a == 's3':
-                    for c,d in b.items():
-                        if c == 'bucket':
-                            for e,f in d.items():
-                                if e == 'name':
-                                    bucket = f
-                                    print(bucket)'''
-    
+        
     #Retrieve object key
-    #s3_details = records_body.get('s3')
+    #key = urllib.parse.unquote_plus(body_in_json['Records'][0]['s3']['object']['key'], encoding='utf-8')
     s3_object = s3_details.get('object')
     key = s3_object.get('key')
 
-    #key = urllib.parse.unquote_plus(body_in_json['Records'][0]['s3']['object']['key'], encoding='utf-8')
-    '''for j in body_in_json.values():
-        for x in j:
-            #print(x)
-            for a,b in x.items():
-                if a == 's3':
-                    for c,d in b.items():
-                        if c == 'object':
-                            for e,f in d.items():
-                                if e == 'key':
-                                    key = f
-                                    print(key)'''
-    
     csv_object_name = "csv-object-of-" + key
     
     try:
@@ -91,26 +49,6 @@ def lambda_handler(event, context):
         buildCSV += "\n"
         buildCSV += ",".join(body.values())
         write_to_bucket(csv_bucket_name, buildCSV, csv_object_name)
-
-        '''for c in body.keys():
-            buildCSV += str(c) + ","
-        buildCSV += "\n"
-        for d in body.values():
-            buildCSV += str(d) + ","'''
-        
-        
-        #return response['ContentType']
-
-        '''
-        response = s3.get_object(Bucket=bucket, Key=key)
-        body = json.loads(response['Body'].read().decode("utf-8"))
-        for v,k in body.items():
-            buildCSV += str(v) + "," + str(k)
-            buildCSV += "\n"
-        print(buildCSV)
-        write_to_bucket(csv_bucket_name, buildCSV, csv_object_name)
-        #return response['ContentType']
-        '''
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
