ca4e0f71dc9b757e87c9f7b50f92a4c07d607dcb
Babajide Oni <127896618+jideoni@users.noreply.github.com>
2023-10-30 02:25:11 +0100

    d

--- main.tf
+++ main.tf
@@ -140,9 +140,9 @@ data "aws_iam_policy_document" "allow_lambda_to_receiveSQSMessage" {
   statement {
     effect    = "Allow"
     actions   = [
-      "sqs:ReceiveMessage",
-      "sqs:DeleteMessage",
-      "sqs:GetQueueAttributes",
+      "SQS:ReceiveMessage",
+      "SQS:DeleteMessage",
+      "SQS:GetQueueAttributes",
     ]
     resources = [aws_sqs_queue.JSON_event_queue.arn]
 
